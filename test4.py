e29e2d1f95f79c6f9999dfdf70fb2f7a5f082b6a
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-07-02 13:17:57 +0300

    add new code 20724

--- test4.py
+++ test4.py
@@ -6,7 +6,20 @@ data = {'value':[1, 2, 3, 3, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10, 55]}
 df = pd.DataFrame(data)
 
 #df['value'].hist()
-#df.boxplot(column='value')
-#plt.show()
+df.boxplot(column='value')
+plt.show()
 
-print(df.describe())
+#print(df.describe())
+
+Q1 = df['value'].quantile(0.25)
+Q3 = df['value'].quantile(0.75)
+
+IQR = Q3 - Q1
+
+downside = Q1 - 1.5 * IQR
+upside = Q3 + 1.5 * IQR
+
+df_new = df[(df['value'] >= downside) & (df['value'] <= upside)]
+
+df_new.boxplot(column='value')
+plt.show()
